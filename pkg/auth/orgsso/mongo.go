f0b1d9fd72bfb8db18dc362e059964f30dc29174
devthejo <devthejo@protonmail.com>
2026-07-05 15:16:58 +0000

    refactor(cloudsched,orgsso): converge get/update/delete boilerplate onto mongoutil helpers

--- pkg/auth/orgsso/mongo.go
+++ pkg/auth/orgsso/mongo.go
@@ -2,7 +2,6 @@ package orgsso
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -75,41 +74,16 @@ func (s *MongoStore) Create(ctx context.Context, p OrgSSOProvider) error {
 }
 
 func (s *MongoStore) Get(ctx context.Context, id string) (OrgSSOProvider, error) {
-	var p OrgSSOProvider
-	err := s.coll.FindOne(ctx, bson.M{"_id": id}).Decode(&p)
-	if errors.Is(err, mongo.ErrNoDocuments) {
-		return OrgSSOProvider{}, ErrNotFound
-	}
-	if err != nil {
-		return OrgSSOProvider{}, fmt.Errorf("orgsso: get provider: %w", err)
-	}
-	return p, nil
+	return mongoutil.FindOne[OrgSSOProvider](ctx, s.coll, bson.M{"_id": id}, ErrNotFound, "orgsso: get provider")
 }
 
 func (s *MongoStore) Update(ctx context.Context, p OrgSSOProvider) error {
 	p.Normalize()
-	res, err := s.coll.ReplaceOne(ctx, bson.M{"_id": p.ID}, p)
-	if err != nil {
-		if mongoutil.IsDuplicateKey(err) {
-			return ErrExists
-		}
-		return fmt.Errorf("orgsso: update provider: %w", err)
-	}
-	if res.MatchedCount == 0 {
-		return ErrNotFound
-	}
-	return nil
+	return mongoutil.ReplaceOneChecked(ctx, s.coll, bson.M{"_id": p.ID}, p, ErrExists, ErrNotFound, "orgsso: update provider")
 }
 
 func (s *MongoStore) Delete(ctx context.Context, id string) error {
-	res, err := s.coll.DeleteOne(ctx, bson.M{"_id": id})
-	if err != nil {
-		return fmt.Errorf("orgsso: delete provider: %w", err)
-	}
-	if res.DeletedCount == 0 {
-		return ErrNotFound
-	}
-	return nil
+	return mongoutil.DeleteOneChecked(ctx, s.coll, bson.M{"_id": id}, ErrNotFound, "orgsso: delete provider")
 }
 
 func (s *MongoStore) ListByTenant(ctx context.Context, tenantID string) ([]OrgSSOProvider, error) {
